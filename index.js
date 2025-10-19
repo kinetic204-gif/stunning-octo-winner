34ff9b6b89397b0c4cebc0b87d8caea488f0d1bf
Kinetic204 <Redbird204@hotmail.com>
2025-10-18 23:37:21 -0400

    Updated Java

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 console.log("JavaScript is linked successfully.");
+
+function openMenu() {
+    document.body.classList.add("menu--open");
+}
+
+function closeMenu() {
+    document.body.classList.remove("menu--open");
+}
+
